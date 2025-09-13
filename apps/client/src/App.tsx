f1457a6bf066882c60447506d894aba883b06694
joaoc-dev <contact@joao-carvalho.dev>
2025-09-13 17:13:33 +0100

    feat: add basic connection between client and server

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { hc } from 'hono/client';
+import type { AppType } from '../../server/src/app';
+import { useEffect, useState } from 'react';
+
+const client = hc<AppType>('http://localhost:3000/');
+
+const getHello = async () => {
+  const res = await client.api.hello.$get({
+    query: {
+      name: 'John Doe',
+    },
+  });
+  return await res.json();
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hello, setHello] = useState<string>('');
+
+  useEffect(() => {
+    const hello = async () => {
+      const hello = await getHello();
+      setHello(hello.message);
+    };
+    hello();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World!</h1>
+      <p>{hello}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,19 @@
 import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'));
+app.use('/api/*', cors());
+app.get('/api', (c) => c.text('Hello Hono!'));
+
+const helloRoute = app.get(
+  '/api/hello',
+  zValidator('query', z.object({ name: z.string() })),
+  (c) => c.json({ message: `Hello ${c.req.valid('query').name}!` })
+);
+
+export type AppType = typeof helloRoute;
 
 export default app;
